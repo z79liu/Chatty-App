ca7f1aafeab5b030346d9a199434bf5659e4385e
Zeyu <haozai_03@hotmail.com>
2019-04-09 23:56:11 +0000

    included websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,51 @@ class App extends Component {
         }
       ]
     }
+    this.addMessage = this.addMessage.bind(this);
 
 
   }
+  
+
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
+
+    this.socket = new WebSocket('ws://localhost:3001');
+
+    // This event listener is fired when the socket is opened (i.e. connected)
+    this.socket.onopen = () => {
+      console.log('Connected to server');
+      this.socket.send('hello')
+    };
+
+    
+    // This event listener is fired whenever the socket receives a message from the server
+    // The parameter e is a MessageEvent which contains the message from the server along with some metadata.
+    this.socket.onmessage = e => {
+      // the actual message from the server is contained in the `data` key
+      console.log(`Got message from the server: ${e.data}`);
+    };
+
+    // This event listener is fired when the socket is closed (either by us or the server)
+    this.onclose = () => {
+      console.log('Client disconnected');
+    };
   }
+
+
+  addMessage(msg) {
+    const newmsg = {username: this.state.currentUser.name, content: msg, id: this.state.messages[this.state.messages.length-1].id + 1 }
+    this.setState({ messages: [...this.state.messages, newmsg] });
+ }
 
   render() {
 
@@ -43,7 +73,7 @@ class App extends Component {
     <div>
       {/* <h1>Hello React :)</h1> */}
       <MessageList messageList = {this.state.messages}/>
-      <ChatBar currentUser = {this.state.currentUser.name}/>
+      <ChatBar addMessage = {this.addMessage} currentUser = {this.state.currentUser.name}/>
     </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
-const ChatBar = (props) => (
-  <footer className="chatbar">
-    <input className="chatbar-username" placeholder="Your Name (Optional)" value ={props.currentUser}/>
-    <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-  </footer>
-)
+class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    // this.keyPress = this.keyPress.bind(this)
+  }
 
+  keyPress(e){
+    if(e.charCode == 13){
+       console.log('value', e.target.value);
+       this.props.addMessage(e.target.value)
+       e.target.value = ""
+    }
+ }
+
+  render() {
+    return (
+    <footer className="chatbar">
+      <input className="chatbar-username" placeholder="Your Name (Optional)" value ={this.props.currentUser}/>
+      <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={( e )=> this.keyPress(e) }/>
+    </footer>
+    )
+  }
+}
 export default ChatBar;
